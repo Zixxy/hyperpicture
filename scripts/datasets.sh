654bd6d6dccbf7f4541a83c0dfba889b4f054ffa
Sylwester Klocek <sylwekklocek@gmail.com>
2018-11-05 10:26:16 +0100

    fix

--- scripts/datasets.sh
+++ scripts/datasets.sh
@@ -1,13 +1,15 @@
+mkdir ../data
+
 #cifar10
 wget http://pjreddie.com/media/files/cifar.tgz
 tar xzf cifar.tgz
-mv cifar ../data/
+mv cifar/ ../data/
 rm cifar.tgz
 mkdir ../data/cifar/My_outs
 
 #benchmark datasets
 wget https://cv.snu.ac.kr/research/EDSR/benchmark.tar #link provided by https://github.com/fperazzi/proSR
 tar xzf benchmark.tar
-mv benchmark ../data/
+mv benchmark/ ../data/
 rm benchmark.tar
 
